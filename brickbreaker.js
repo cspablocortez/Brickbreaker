d4fe20d42e2cb429a0d540f91f34233e3a60fce9
Pablo Cortez <cspablocortez@gmail.com>
2024-03-23 18:44:31 -0400

    Update brickbreaker.js

--- brickbreaker.js
+++ brickbreaker.js
@@ -1,6 +1,7 @@
 const canvas  = document.getElementById('myCanvas');
 const ctx     = canvas.getContext('2d');
 const playBtn = document.getElementById('runBtn');
+let interval;
 
 let ball = {
     x: canvas.width / 2,
@@ -9,20 +10,48 @@ let ball = {
     dx: 2,
     dy: -2,
     draw: function() {
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2)
-        ctx.fillStyle = 'rgb(76 136 76)'
-        ctx.fill()
-        ctx.closePath()
-        this.x += this.dx
-        this.y += this.dy
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+        ctx.fillStyle = 'rgb(76 136 76)';
+        ctx.fill();
+        ctx.closePath();
+        this.x += this.dx;
+        this.y += this.dy;
 
-        if ((this.y + this.dy) < 0 || (this.y + this.dy) > canvas.height) {
-            this.dy = this.dy * -1
+        
+        if ((this.x + this.dx) < 0 || (this.x + this.dx) > canvas.width) {
+            this.dx = this.dx * -1;
         }
 
-        if ((this.x + this.dx) < 0 || (this.x + this.dx) > canvas.width) {
-            this.dx = this.dx * -1
+        if (this.y + this.dy < this.r) {
+            this.dy = -this.dy;
+        } else if (this.y + this.dy > canvas.height - (this.r / 2)) {
+            alert('GAME OVER');
+            document.location.reload();
+            clearInterval(interval);
+        }
+    }
+}
+
+let paddle = {
+    height: 10,
+    width: 75,
+    x: (canvas.width - 75) / 2,
+    y: canvas.height - 10,
+    dx: 7,
+    rightPressed: false,
+    leftPressed: false,
+    draw: function () {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'rgb(76 136 76)';
+        ctx.fill();
+        ctx.closePath();
+
+        if (this.rightPressed) {
+            this.x = Math.min(this.x + this.dx, canvas.width - this.width);
+        } else if (this.leftPressed) {
+            this.x = Math.max(this.x - this.dx, 0);
         }
     }
 }
@@ -30,10 +59,11 @@ let ball = {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ball.draw();
+    paddle.draw();
 }
 
 function startGame() {
-    const interval = setInterval(draw, 10);
+    interval = setInterval(draw, 10);
     console.log(`Interval: ${interval}`);
 }
 
@@ -41,4 +71,24 @@ playBtn.addEventListener('click', () => {
     startGame();
     playBtn.disabled = true;
     playBtn.classList.add('disabled');
-})
+});
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        paddle.rightPressed = true;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        paddle.rightPressed = false;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.leftPressed = false;
+    }
+}
+
